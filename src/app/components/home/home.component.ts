e96230c5c17423c273456c608307f839a6ce79c6
Mustafa-Seif <mustafaabdella64@gmail.com>
2023-02-21 02:06:24 +0200

    1- add footer icon 2- change some styles

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,12 +9,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class HomeComponent {
    //  REACTIVE FORM
    admissionForm: FormGroup = new FormGroup({
-    name: new FormControl('',[Validators.required,Validators.maxLength(35),Validators.minLength(5)]),
+    name: new FormControl('',[Validators.required,Validators.maxLength(35),Validators.minLength(5),Validators.pattern(/[A-Za-z0-9]/)]),
     email: new FormControl('',[Validators.required, Validators.email]),
-    tel: new FormControl('',[Validators.required]),
+    tel: new FormControl('',[Validators.required,Validators.maxLength(14),Validators.minLength(11)]),
     department: new FormControl('',Validators.required),
     date: new FormControl('',Validators.required),
-    textarea: new FormControl('',Validators.required),
+    textarea: new FormControl('',[Validators.required,Validators.maxLength(200),Validators.minLength(20),Validators.pattern(/[A-Za-z0-9]/)]),
   });
 
   // HANFLE SUBMIT 
